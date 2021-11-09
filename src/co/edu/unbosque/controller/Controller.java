db51ae7cd2f379b1c6ae668b129a805203b469a1
Felipe Rojas <jfrojasr@unbosque.edu.co>
2021-11-09 14:57:32 -0500

    Things

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,8 +1,63 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.model.Algoritmos;
+import co.edu.unbosque.view.Vista;
+
 public class Controller {
-	
+
+	private Vista vista;
+	private Algoritmos algo;
+
 	public Controller() {
-		
+		algo = new Algoritmos();
+		vista = new Vista();
+
+		ejecutar();
+	}
+
+	public void ejecutar() {
+
+		vista.mostrar("Bienvenido al programa de algoritmos de programación dinámica, esocoja una opción: ");
+		String dec = vista.escritura("[1] Algoritmo Floyd\n[2] Problema del viajero\n[3] Multipliación de matrices\n"
+				+ "[4] Problema de asignación de tareas\n[5] Coeficientes binomial\n");
+
+		int decision = Integer.parseInt(dec);
+
+		switch (decision) {
+		case 1:
+			int tamanio = Integer.parseInt(vista.escritura("Ingrese el tamaño de la matriz:\n"));
+			int[][] matriz = new int[tamanio][tamanio];
+			int[][] matriz2 = new int[tamanio][tamanio];
+
+			vista.mostrar("Ingrese los datos:\n");
+			for (int i = 0; i < tamanio; i++) {
+				for (int j = 0; j < tamanio; j++) {
+					matriz[i][j] = Integer.parseInt(vista.escritura(""));
+				}
+			}
+			
+			matriz2 = algo.floydWarshall(matriz, tamanio);
+
+			for (int i = 0; i < tamanio; i++) {
+				for (int j = 0; j < tamanio; j++) {
+					System.out.print(matriz2[i][j]);
+				}
+			}
+
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			break;
+
+		case 4:
+			break;
+
+		case 5:
+			break;
+
+		}
 	}
 }
