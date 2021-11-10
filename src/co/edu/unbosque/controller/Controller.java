9ec6e6b4c45a8a0ba8d3fff7aedc9dc9c592159a
Felipe Rojas <jfrojasr@unbosque.edu.co>
2021-11-10 17:50:53 -0500

    Finshing

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -18,137 +18,171 @@ public class Controller {
 	public void ejecutar() {
 
 		int decision = 0;
-
+		String dec = "";
 		do {
 			vista.mostrar("Bienvenido al programa de algoritmos de programación dinámica, esocoja una opción: ");
-			String dec = vista
-					.escritura("[1] Algoritmo Floyd\n[2] Problema del viajero\n[3] Multipliación de matrices\n"
-							+ "[4] Problema de asignación de tareas\n[5] Coeficientes binomial\n" + "[6] Salir");
+			dec = vista.escritura("[1] Algoritmo Floyd\n[2] Problema del viajero\n[3] Multipliación de matrices\n"
+					+ "[4] Problema de asignación de tareas\n[5] Coeficientes binomial\n" + "[6] Salir");
 			try {
 				if (vista.exceptionNumber(dec)) {
-					decision = Integer.parseInt(dec);
-				}
-
-				switch (decision) {
-				case 1:
-					String t = vista.escritura("Ingrese el tamaño de la matriz:\n");
-					try {
-						if (vista.exceptionNumber(t)) {
-							int tamanio = Integer.parseInt(t);
-							int[][] matriz = new int[tamanio][tamanio];
-							int[][] m = new int[tamanio][tamanio];
-							int inf = 9999;
-
-							vista.mostrar("Ingrese los datos:");
-							for (int i = 0; i < tamanio; i++) {
-								for (int j = 0; j < tamanio; j++) {
-									matriz[i][j] = Integer.parseInt(vista.escritura(""));
-								}
-							}
-
-							m = algo.floydWarshall(matriz, tamanio);
-
-							for (int i = 0; i < tamanio; i++) {
-								for (int j = 0; j < tamanio; j++) {
-									if (m[i][j] == 99)
-										System.out.print("INF ");
-									else
-										System.out.print(m[i][j] + " ");
-								}
-								System.out.println("\n");
-							}
-
-						}
-					} catch (Exception e1) {
-						vista.mostrar("Señor Usuario, escriba bien los números\n");
-					}
-
-					break;
-
-				case 2:
-					break;
-
-				case 3:
-
-					String t3 = vista.escritura("Ingrese el tamaño del arreglo:");
-					try {
-						if (vista.exceptionNumber(t3)) {
-							int tamanio3 = Integer.parseInt(t3);
-							int[] arreglo = new int[tamanio3];
-
-							vista.mostrar("Ingrese los datos:");
-							for (int i = 0; i < tamanio3; i++) {
-								arreglo[i] = Integer.parseInt(vista.escritura(""));
-							}
-							int prueba = algo.MatrixChainOrder(arreglo, tamanio3);
-							vista.mostrar("" + prueba);
-
-						}
-					} catch (Exception e) {
-						vista.mostrar("Señor Usuario, escriba bien los números\n");
-					}
-
-					break;
-
-				case 4:
-
-					String d = vista.escritura("Ingrese el número de días:");
-					try {
-						if (vista.exceptionNumber(d)) {
-							int dias = Integer.parseInt(d);
-							int[] high = new int[dias];
-							int[] low = new int[dias];
-							vista.mostrar("Ingrese el número máximo de trabajos:");
-							for (int i = 0; i < high.length; i++) {
-								high[i] = Integer.parseInt(vista.escritura(""));
-							}
-
-							vista.mostrar("Se han guardado el máximo de trabajos.\n");
-
-							vista.mostrar("Ingrese el número mínimo de trabajos:");
-							for (int j = 0; j < low.length; j++) {
-								low[j] = Integer.parseInt(vista.escritura(""));
-							}
-							vista.mostrar("Se han guardado el mínimo de trabajos.\n");
-
-							int tareas = algo.maxTasks(high, low, dias);
-
-							vista.mostrar("La cantidad de tareas asignadas son: " + tareas);
-						}
-					} catch (Exception e) {
-						vista.mostrar("Señor Usuario, escriba bien los números\n");
-					}
-
-					break;
-
-				case 5:
-
-					String c1 = vista.escritura("Ingrese el tamaño para el coeficiente numero 1:");
-					String c2 = vista.escritura("Ingrese el tamaño para el coeficiente numero 2:");
-
-					try {
-						if (vista.exceptionNumber(c1) && vista.exceptionNumber(c2)) {
-							int n = Integer.parseInt(c1);
-							int k = Integer.parseInt(c2);
-							int resultado = algo.binomialCoeff(n, k);
-
-							vista.mostrar("" + resultado);
-						}
-					} catch (Exception e) {
-						vista.mostrar("Señor Usuario, escriba bien los números\n");
-					}
-
-					break;
-				case 6:
-					System.exit(0);
-					break;
-
 				}
 			} catch (Exception e1) {
 				vista.mostrar("Señor Usuario, escriba bien los números\n");
+
 			}
 
-		} while (decision != 6);
+			switch (dec) {
+			case "1":
+				String t = vista.escritura("Ingrese el tamaño de la matriz:\n");
+				try {
+					if (vista.exceptionNumber(t)) {
+						int tamanio = Integer.parseInt(t);
+						int[][] matriz = new int[tamanio][tamanio];
+						int[][] m = new int[tamanio][tamanio];
+						int inf = 9999;
+
+						vista.mostrar("Ingrese los datos:");
+						for (int i = 0; i < tamanio; i++) {
+							for (int j = 0; j < tamanio; j++) {
+								matriz[i][j] = Integer.parseInt(vista.escritura(""));
+							}
+						}
+
+						m = algo.floydWarshall(matriz, tamanio);
+
+						for (int i = 0; i < tamanio; i++) {
+							for (int j = 0; j < tamanio; j++) {
+								if (m[i][j] == 99)
+									System.out.print("INF ");
+								else
+									System.out.print(m[i][j] + " ");
+							}
+							System.out.println("\n");
+						}
+
+					}
+				} catch (Exception e1) {
+					vista.mostrar("Señor Usuario, escriba bien los números\n");
+				}
+
+				break;
+
+			case "2":
+
+				String n1 = vista.escritura("Escriba el número de las ciudades: ");
+				try {
+					if (vista.exceptionNumber(n1)) {
+						int n = Integer.parseInt(n1);
+						int[][] c = new int[n][n];
+						int[] tour = new int[10];
+
+						vista.mostrar("Escriba el costo de las ciudades: ");
+						for (int i = 0; i < n; i++) {
+							for (int j = 0; j < n; j++) {
+								c[i][j] = Integer.parseInt(vista.escritura(""));
+							}
+						}
+						for (int i = 0; i < n; i++) {
+							tour[i] = i;
+						}
+
+						int result = algo.tspdp(c, tour, 1, n);
+
+						vista.mostrar("El tour más optimo es: ");
+						for (int i = 1; i <= n; i++) {
+
+							System.out.print(tour[i] + " -> ");
+						}
+
+						vista.mostrar("1");
+
+						vista.mostrar("El costo mínimo es: " + result + "\n");
+					}
+
+				} catch (Exception e) {
+					vista.mostrar("Señor Usuario, escriba bien los números\n");
+				}
+
+				break;
+
+			case "3":
+
+				String t3 = vista.escritura("Ingrese el tamaño del arreglo:");
+				try {
+					if (vista.exceptionNumber(t3)) {
+						int tamanio3 = Integer.parseInt(t3);
+						int[] arreglo = new int[tamanio3];
+
+						vista.mostrar("Ingrese los datos:");
+						for (int i = 0; i < tamanio3; i++) {
+							arreglo[i] = Integer.parseInt(vista.escritura(""));
+						}
+						int prueba = algo.MatrixChainOrder(arreglo, tamanio3);
+						vista.mostrar("" + prueba);
+
+					}
+				} catch (Exception e) {
+					vista.mostrar("Señor Usuario, escriba bien los números\n");
+				}
+
+				break;
+
+			case "4":
+
+				String d = vista.escritura("Ingrese el número de días:");
+				try {
+					if (vista.exceptionNumber(d)) {
+						int dias = Integer.parseInt(d);
+						int[] high = new int[dias];
+						int[] low = new int[dias];
+						vista.mostrar("Ingrese el número máximo de trabajos:");
+						for (int i = 0; i < high.length; i++) {
+							high[i] = Integer.parseInt(vista.escritura(""));
+						}
+
+						vista.mostrar("Se han guardado el máximo de trabajos.\n");
+
+						vista.mostrar("Ingrese el número mínimo de trabajos:");
+						for (int j = 0; j < low.length; j++) {
+							low[j] = Integer.parseInt(vista.escritura(""));
+						}
+						vista.mostrar("Se han guardado el mínimo de trabajos.\n");
+
+						int tareas = algo.maxTasks(high, low, dias);
+
+						vista.mostrar("La cantidad de tareas asignadas son: " + tareas);
+					}
+				} catch (Exception e) {
+					vista.mostrar("Señor Usuario, escriba bien los números\n");
+				}
+
+				break;
+
+			case "5":
+
+				String c1 = vista.escritura("Ingrese el tamaño para el coeficiente numero 1:");
+				String c2 = vista.escritura("Ingrese el tamaño para el coeficiente numero 2:");
+
+				try {
+					if (vista.exceptionNumber(c1) && vista.exceptionNumber(c2)) {
+						int n = Integer.parseInt(c1);
+						int k = Integer.parseInt(c2);
+						int resultado = algo.binomialCoeff(n, k);
+
+						vista.mostrar("" + resultado);
+					}
+				} catch (Exception e) {
+					vista.mostrar("Señor Usuario, escriba bien los números\n");
+				}
+
+				break;
+			case "6":
+				break;
+			default:
+				vista.mostrar("Decisión incorrecta");
+			}
+
+		} while (!dec.equals("6"));
 
 	}
 }
--- src/co/edu/unbosque/model/Algoritmos.java
+++ src/co/edu/unbosque/model/Algoritmos.java
@@ -61,20 +61,17 @@ public class Algoritmos {
 	// Asignación de tareas
 	public int maxTasks(int[] high, int[] low, int n) {
 
-	    int[] task_dp = new int[n + 1];
-	 
-	    task_dp[0] = 0;
-	 
-	    task_dp[1] = high[0];
-	 
-	    for (int i = 2; i <= n; i++) {
-	        task_dp[i] = Math.max(high[i - 1] + task_dp[i - 2],
-	                        low[i - 1] + task_dp[i - 1]);
-	    }
-	    return task_dp[n];
+		int[] task_dp = new int[n + 1];
+
+		task_dp[0] = 0;
+
+		task_dp[1] = high[0];
+
+		for (int i = 2; i <= n; i++) {
+			task_dp[i] = Math.max(high[i - 1] + task_dp[i - 2], low[i - 1] + task_dp[i - 1]);
+		}
+		return task_dp[n];
 	}
-	
-	
 
 	// Coeficiente binomial
 	public int binomialCoeff(int n, int k) {
@@ -96,51 +93,45 @@ public class Algoritmos {
 		return (a < b) ? a : b;
 	}
 	
-	
-	
-	public int tspdp(int c[][], int tour[], int start, int n)
-
-	{
+	//Viajero
+	public int tspdp(int c[][], int tour[], int start, int n) {
 
 		int mintour[] = new int[10], temp[] = new int[10], mincost = 999, ccost, i, j, k;
 
-		if (start == n - 1)
-
-		{
+		if (start == n - 1) {
 
 			return (c[tour[n - 1]][tour[n]] + c[tour[n]][1]);
 
 		}
 
-		for (i = start + 1; i <= n; i++)
+		for (i = start + 1; i <= n; i++) {
 
-		{
-
-			for (j = 1; j <= n; j++)
+			for (j = 1; j <= n; j++) {
 
 				temp[j] = tour[j];
+			}
 
 			temp[start + 1] = tour[i];
 
 			temp[i] = tour[start + 1];
 
-			if ((c[tour[start]][tour[i]] + (ccost = tspdp(c, temp, start + 1, n))) < mincost)
-
-			{
+			if ((c[tour[start]][tour[i]] + (ccost = tspdp(c, temp, start + 1, n))) < mincost) {
 
 				mincost = c[tour[start]][tour[i]] + ccost;
 
-				for (k = 1; k <= n; k++)
+				for (k = 1; k <= n; k++) {
 
 					mintour[k] = temp[k];
+				}
 
 			}
 
 		}
 
-		for (i = 1; i <= n; i++)
+		for (i = 1; i <= n; i++) {
 
 			tour[i] = mintour[i];
+		}
 
 		return mincost;
 
