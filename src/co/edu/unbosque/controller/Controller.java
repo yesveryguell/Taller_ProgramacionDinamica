aa6162c68890e5f4e4ed64d626be5a8594d803fe
Felipe Rojas <jfrojasr@unbosque.edu.co>
2021-11-10 16:51:46 -0500

    Validations

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -17,92 +17,138 @@ public class Controller {
 
 	public void ejecutar() {
 
-		vista.mostrar("Bienvenido al programa de algoritmos de programación dinámica, esocoja una opción: ");
-		String dec = vista.escritura("[1] Algoritmo Floyd\n[2] Problema del viajero\n[3] Multipliación de matrices\n"
-				+ "[4] Problema de asignación de tareas\n[5] Coeficientes binomial\n");
+		int decision = 0;
 
-		int decision = Integer.parseInt(dec);
-
-		switch (decision) {
-		case 1:
-			int tamanio = Integer.parseInt(vista.escritura("Ingrese el tamaño de la matriz:\n"));
-			int[][] matriz = new int[tamanio][tamanio];
-			int[][] m = new int[tamanio][tamanio];
-			int inf = 9999;
-
-			vista.mostrar("Ingrese los datos:");
-			for (int i = 0; i < tamanio; i++) {
-				for (int j = 0; j < tamanio; j++) {
-					matriz[i][j] = Integer.parseInt(vista.escritura(""));
+		do {
+			vista.mostrar("Bienvenido al programa de algoritmos de programación dinámica, esocoja una opción: ");
+			String dec = vista
+					.escritura("[1] Algoritmo Floyd\n[2] Problema del viajero\n[3] Multipliación de matrices\n"
+							+ "[4] Problema de asignación de tareas\n[5] Coeficientes binomial\n" + "[6] Salir");
+			try {
+				if (vista.exceptionNumber(dec)) {
+					decision = Integer.parseInt(dec);
 				}
-			}
 
-			m = algo.floydWarshall(matriz, tamanio);
+				switch (decision) {
+				case 1:
+					String t = vista.escritura("Ingrese el tamaño de la matriz:\n");
+					try {
+						if (vista.exceptionNumber(t)) {
+							int tamanio = Integer.parseInt(t);
+							int[][] matriz = new int[tamanio][tamanio];
+							int[][] m = new int[tamanio][tamanio];
+							int inf = 9999;
 
-			for (int i = 0; i < tamanio; i++) {
-				for (int j = 0; j < tamanio; j++) {
-					if (m[i][j] == 99)
-						System.out.print("INF ");
-					else
-						System.out.print(m[i][j] + " ");
+							vista.mostrar("Ingrese los datos:");
+							for (int i = 0; i < tamanio; i++) {
+								for (int j = 0; j < tamanio; j++) {
+									matriz[i][j] = Integer.parseInt(vista.escritura(""));
+								}
+							}
+
+							m = algo.floydWarshall(matriz, tamanio);
+
+							for (int i = 0; i < tamanio; i++) {
+								for (int j = 0; j < tamanio; j++) {
+									if (m[i][j] == 99)
+										System.out.print("INF ");
+									else
+										System.out.print(m[i][j] + " ");
+								}
+								System.out.println("\n");
+							}
+
+						}
+					} catch (Exception e1) {
+						vista.mostrar("Señor Usuario, escriba bien los números\n");
+					}
+
+					break;
+
+				case 2:
+					break;
+
+				case 3:
+
+					String t3 = vista.escritura("Ingrese el tamaño del arreglo:");
+					try {
+						if (vista.exceptionNumber(t3)) {
+							int tamanio3 = Integer.parseInt(t3);
+							int[] arreglo = new int[tamanio3];
+
+							vista.mostrar("Ingrese los datos:");
+							for (int i = 0; i < tamanio3; i++) {
+								arreglo[i] = Integer.parseInt(vista.escritura(""));
+							}
+							int prueba = algo.MatrixChainOrder(arreglo, tamanio3);
+							vista.mostrar("" + prueba);
+
+						}
+					} catch (Exception e) {
+						vista.mostrar("Señor Usuario, escriba bien los números\n");
+					}
+
+					break;
+
+				case 4:
+
+					String d = vista.escritura("Ingrese el número de días:");
+					try {
+						if (vista.exceptionNumber(d)) {
+							int dias = Integer.parseInt(d);
+							int[] high = new int[dias];
+							int[] low = new int[dias];
+							vista.mostrar("Ingrese el número máximo de trabajos:");
+							for (int i = 0; i < high.length; i++) {
+								high[i] = Integer.parseInt(vista.escritura(""));
+							}
+
+							vista.mostrar("Se han guardado el máximo de trabajos.\n");
+
+							vista.mostrar("Ingrese el número mínimo de trabajos:");
+							for (int j = 0; j < low.length; j++) {
+								low[j] = Integer.parseInt(vista.escritura(""));
+							}
+							vista.mostrar("Se han guardado el mínimo de trabajos.\n");
+
+							int tareas = algo.maxTasks(high, low, dias);
+
+							vista.mostrar("La cantidad de tareas asignadas son: " + tareas);
+						}
+					} catch (Exception e) {
+						vista.mostrar("Señor Usuario, escriba bien los números\n");
+					}
+
+					break;
+
+				case 5:
+
+					String c1 = vista.escritura("Ingrese el tamaño para el coeficiente numero 1:");
+					String c2 = vista.escritura("Ingrese el tamaño para el coeficiente numero 2:");
+
+					try {
+						if (vista.exceptionNumber(c1) && vista.exceptionNumber(c2)) {
+							int n = Integer.parseInt(c1);
+							int k = Integer.parseInt(c2);
+							int resultado = algo.binomialCoeff(n, k);
+
+							vista.mostrar("" + resultado);
+						}
+					} catch (Exception e) {
+						vista.mostrar("Señor Usuario, escriba bien los números\n");
+					}
+
+					break;
+				case 6:
+					System.exit(0);
+					break;
+
 				}
-				System.out.println("\n");
+			} catch (Exception e1) {
+				vista.mostrar("Señor Usuario, escriba bien los números\n");
 			}
 
-			break;
+		} while (decision != 6);
 
-		case 2:
-			break;
-
-		case 3:
-
-			int tamanio3 = Integer.parseInt(vista.escritura("Ingrese el tamaño del arreglo:"));
-			int[] arreglo = new int[tamanio3];
-
-			vista.mostrar("Ingrese los datos:");
-			for (int i = 0; i < tamanio3; i++) {
-				arreglo[i] = Integer.parseInt(vista.escritura(""));
-			}
-			int prueba = algo.MatrixChainOrder(arreglo, tamanio3);
-			vista.mostrar("" + prueba);
-
-			break;
-
-		case 4:
-
-			int dias = Integer.parseInt(vista.escritura("Ingrese el número de días:"));
-			int[] high = new int[dias];
-			int[] low = new int[dias];
-			vista.mostrar("Ingrese el número máximo de trabajos:");
-			for (int i = 0; i < high.length; i++) {
-				high[i] = Integer.parseInt(vista.escritura(""));
-			}
-
-			vista.mostrar("Se han guardado el máximo de trabajos.\n");
-
-			vista.mostrar("Ingrese el número mínimo de trabajos:");
-			for (int j = 0; j < low.length; j++) {
-				low[j] = Integer.parseInt(vista.escritura(""));
-			}
-			vista.mostrar("Se han guardado el mínimo de trabajos.\n");
-
-			int tareas = algo.maxTasks(high, low, dias);
-
-			vista.mostrar("La cantidad de tareas asignadas son: " + tareas);
-
-			break;
-
-		case 5:
-
-			int n = Integer.parseInt(vista.escritura("Ingrese el tamaño para el coeficiente numero 1:"));
-			int k = Integer.parseInt(vista.escritura("Ingrese el tamaño para el coeficiente numero 2:"));
-
-			int resultado = algo.binomialCoeff(n, k);
-
-			vista.mostrar("" + resultado);
-
-			break;
-
-		}
 	}
 }
--- src/co/edu/unbosque/view/Vista.java
+++ src/co/edu/unbosque/view/Vista.java
@@ -4,18 +4,39 @@ import java.util.Scanner;
 
 public class Vista {
 	Scanner sc = new Scanner(System.in);
-	
+
 	public Vista() {
-		
+
 	}
-	
+
 	public String escritura(String mensaje) {
 		System.out.println(mensaje);
 		String escrito = sc.nextLine();
 		return escrito;
 	}
-	
+
 	public void mostrar(String mensaje) {
 		System.out.println(mensaje);
+	}
+
+	public boolean soloNumeros(String pNumber) {
+
+		boolean ver = true;
+
+		if (!pNumber.trim().matches("[0-9]")) {
+			ver = false;
+		}
+		return ver;
+	}
+
+	public boolean exceptionNumber(String pNumber) throws Exception {
+		boolean ver;
+
+		if (soloNumeros(pNumber)) {
+			ver = true;
+		} else {
+			throw new Exception("Age not valid, try again (age cannot be greater than 100)");
+		}
+		return ver;
 	}
 }
